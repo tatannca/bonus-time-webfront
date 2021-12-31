5a0333c7d91df8e0737f0583672f48737340852d
tateno <gd28.c.tateno@gmail.com>
2021-12-31 21:50:41 +0900

    アカウント登録とログイン機能追加

--- firebase/config.ts
+++ firebase/config.ts
@@ -19,4 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
+export const firebaseAuth = getAuth(app)
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,39 +1,73 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import axios from 'axios'
-import { Container, VStack, Input, Button, IconButton, Heading } from '@chakra-ui/react'
+import { Container, VStack, Input, InputGroup, Button, IconButton, InputRightElement, Heading } from '@chakra-ui/react'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { firebaseAuth } from '../../firebase/config'
+import { useState } from 'react'
 
 const SingUp: NextPage = () => {
   const router = useRouter()
   const pageBack = () => router.back()
-  const apiTest = () => {
-    axios
-      .get('http://localhost:8080/api/v1/public')
-      .then((res) => {
-        console.log(res.data)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPass, setShowPass] = useState(false)
+
+  const login = () => {
+    signInWithEmailAndPassword(firebaseAuth, email, password)
+      .then(async (res) => {
+        const user = res.user
+        const token = await user.getIdToken()
+        localStorage.setItem('access_token', token)
+        console.log('user', user)
+        console.log('token', token)
+        router.push('/dashboard')
       })
       .catch((err) => {
-        console.log(err)
+        const errorCode = err.code
+        const errorMessage = err.message
+        console.log(errorCode, errorMessage)
       })
   }
 
+  // const apiTest = () => {
+  //   axios
+  //     .get('http://localhost:8080/api/v1/public')
+  //     .then((res) => {
+  //       console.log(res.data)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }
+
   return (
-    <>
-      <Container>
-        <IconButton as="a" my={3} aria-label="戻る" icon={<ChevronLeftIcon />} onClick={pageBack} />
-        <VStack spacing={10}>
-          <Heading as="h1">ログイン</Heading>
-          <VStack spacing={5} w="100%">
-            <Input placeholder="メールアドレス" type="email" />
-            <Input placeholder="パスワード" type="password" />
-          </VStack>
-          <Button colorScheme="teal" onClick={apiTest}>
-            ログイン
-          </Button>
+    <Container>
+      <IconButton as="a" my={3} aria-label="戻る" icon={<ChevronLeftIcon />} onClick={pageBack} />
+      <VStack spacing={10}>
+        <Heading as="h1">ログイン</Heading>
+        <VStack spacing={5} w="100%">
+          <Input placeholder="メールアドレス" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <InputGroup>
+            <Input
+              placeholder="パスワード"
+              type={showPass ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <InputRightElement w="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={() => setShowPass((prev) => !prev)}>
+                {showPass ? '非表示' : '表示'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
         </VStack>
-      </Container>
-    </>
+        <Button colorScheme="teal" onClick={login}>
+          ログイン
+        </Button>
+      </VStack>
+    </Container>
   )
 }
 export default SingUp
