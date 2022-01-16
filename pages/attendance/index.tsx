48f432c71d502b63ca1b57e0f6be79cfb7f8d2d8
tateno <gd28.c.tateno@gmail.com>
2022-01-16 15:45:01 +0900

    wip

--- pages/attendance/index.tsx
+++ pages/attendance/index.tsx
@@ -3,10 +3,44 @@ import { useEffect, useState } from 'react';
 import { User } from 'firebase/auth';
 import { firebaseAuth } from '../../firebase/config';
 import { useRouter } from 'next/router';
-import { Container, Text, Table, Thead, Tbody, Tr, Th, Td, Flex, Spacer, IconButton } from '@chakra-ui/react';
+import { Container, Text, Table, Thead, Tbody, Tr, Th, Td, Flex, Spacer, IconButton, Box } from '@chakra-ui/react';
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
 
 type TableRowProps = {
+  month: string;
+  attendance: {
+    date: string;
+    startTime: string;
+    endTime: string;
+    restTime: string;
+    workingHours: string;
+  }[];
+};
+
+const sampleData: TableRowProps[] = [
+  {
+    month: '2022-01',
+    attendance: [
+      { date: '1/10', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '1/11', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '1/12', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '1/13', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '1/14', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' }
+    ]
+  },
+  {
+    month: '2021-12',
+    attendance: [
+      { date: '12/10', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '12/11', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '12/12', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '12/13', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+      { date: '12/14', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' }
+    ]
+  }
+];
+
+type dataRow = {
   date: string;
   startTime: string;
   endTime: string;
@@ -14,15 +48,7 @@ type TableRowProps = {
   workingHours: string;
 };
 
-const sampleData: TableRowProps[] = [
-  { date: '10/10', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
-  { date: '10/11', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
-  { date: '10/12', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
-  { date: '10/13', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
-  { date: '10/14', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' }
-];
-
-const TableRow: NextPage<TableRowProps> = (props) => {
+const TableRow: NextPage<dataRow> = (props) => {
   return (
     <Tr>
       <Td>{props.date}</Td>
@@ -50,35 +76,70 @@ const Attendance: NextPage = () => {
     return () => unsubscribe();
   }, [router]);
 
+  const [currentMonth, setCurrentMonth] = useState(0);
+  const [disabledPushPrev, setDisabledPushPrev] = useState(false);
+  const [disabledPushNext, setDisabledPushNext] = useState(true);
+
+  const prevMonth = () => {
+    if (currentMonth !== sampleData.length - 1) {
+      setCurrentMonth((prev) => prev + 1);
+      setDisabledPushNext(false);
+    } else {
+      setDisabledPushPrev(true);
+    }
+  };
+
+  const nextMonth = () => {
+    if (currentMonth !== 0) {
+      setCurrentMonth((prev) => prev - 1);
+      setDisabledPushPrev(false);
+    } else {
+      setDisabledPushNext(true);
+    }
+  };
+
   if (!user) return <></>;
 
   return (
     <Container maxW="container.sm">
       <Flex alignItems="center" justifyContent="center">
-        <IconButton aria-label="前月へ" icon={<ArrowLeftIcon />} />
+        <IconButton
+          onClick={prevMonth}
+          disabled={disabledPushPrev || currentMonth === sampleData.length - 1}
+          aria-label="前月へ"
+          icon={<ArrowLeftIcon />}
+        />
         <Spacer />
         <Text py={5} textAlign="center">
-          今月
+          {sampleData[currentMonth].month}
         </Text>
         <Spacer />
-        <IconButton aria-label="翌月へ" icon={<ArrowRightIcon />} />
+        <IconButton
+          onClick={nextMonth}
+          disabled={disabledPushNext || currentMonth === 0}
+          aria-label="翌月へ"
+          icon={<ArrowRightIcon />}
+        />
       </Flex>
-      <Table variant="striped" size="sm">
-        <Thead>
-          <Tr>
-            <Th>日付</Th>
-            <Th>出勤</Th>
-            <Th>退勤</Th>
-            <Th>休憩</Th>
-            <Th>就業時間</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {sampleData.map((data, i) => (
-            <TableRow key={i.toString()} {...data} />
-          ))}
-        </Tbody>
-      </Table>
+
+      <Box overflow="scroll">
+        <Table variant="striped" size="sm">
+          <Thead>
+            <Tr>
+              <Th>日付</Th>
+              <Th>出勤</Th>
+              <Th>退勤</Th>
+              <Th>休憩</Th>
+              <Th>就業時間</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {sampleData[currentMonth].attendance.map((data, i) => (
+              <TableRow key={i.toString()} {...data} />
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
     </Container>
   );
 };
