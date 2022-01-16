c52a65a34190b51d513ba6d586d85d7efcf8242b
tateno <gd28.c.tateno@gmail.com>
2022-01-16 13:44:34 +0900

    wip

--- pages/attendance/index.tsx
+++ pages/attendance/index.tsx
@@ -3,7 +3,36 @@ import { useEffect, useState } from 'react';
 import { User } from 'firebase/auth';
 import { firebaseAuth } from '../../firebase/config';
 import { useRouter } from 'next/router';
-import { Container, Text, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import { Container, Text, Table, Thead, Tbody, Tr, Th, Td, Flex, Spacer, IconButton } from '@chakra-ui/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
+
+type TableRowProps = {
+  date: string;
+  startTime: string;
+  endTime: string;
+  restTime: string;
+  workingHours: string;
+};
+
+const sampleData: TableRowProps[] = [
+  { date: '10/10', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+  { date: '10/11', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+  { date: '10/12', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+  { date: '10/13', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' },
+  { date: '10/14', startTime: '10:00', endTime: '19:00', restTime: '1:00', workingHours: '8:00' }
+];
+
+const TableRow: NextPage<TableRowProps> = (props) => {
+  return (
+    <Tr>
+      <Td>{props.date}</Td>
+      <Td>{props.startTime}</Td>
+      <Td>{props.endTime}</Td>
+      <Td>{props.restTime}</Td>
+      <Td>{props.workingHours}</Td>
+    </Tr>
+  );
+};
 
 const Attendance: NextPage = () => {
   const router = useRouter();
@@ -25,9 +54,15 @@ const Attendance: NextPage = () => {
 
   return (
     <Container maxW="container.sm">
-      <Text py={5} textAlign="center">
-        今月
-      </Text>
+      <Flex alignItems="center" justifyContent="center">
+        <IconButton aria-label="前月へ" icon={<ArrowLeftIcon />} />
+        <Spacer />
+        <Text py={5} textAlign="center">
+          今月
+        </Text>
+        <Spacer />
+        <IconButton aria-label="翌月へ" icon={<ArrowRightIcon />} />
+      </Flex>
       <Table variant="striped" size="sm">
         <Thead>
           <Tr>
@@ -39,27 +74,9 @@ const Attendance: NextPage = () => {
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>10/10</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-          </Tr>
-          <Tr>
-            <Td>10/10</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-          </Tr>
-          <Tr>
-            <Td>10/10</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-            <Td>--:--</Td>
-          </Tr>
+          {sampleData.map((data, i) => (
+            <TableRow key={i.toString()} {...data} />
+          ))}
         </Tbody>
       </Table>
     </Container>
