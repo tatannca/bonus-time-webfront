c4689f49067edba5cc1c3b7b29e5e1b91771c753
tateno <gd28.c.tateno@gmail.com>
2022-01-29 20:40:37 +0900

    tokenの更新がうまく行ってないのでこれで様子を見る

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,42 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { store } from '../store';
 import { Provider } from 'react-redux';
-import { firebaseAuth } from '../firebase/config';
+// import { firebaseAuth } from '../firebase/config';
 import { useEffect } from 'react';
+import { getAuth, onIdTokenChanged } from 'firebase/auth';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // JWT更新監視
   useEffect(() => {
-    // JWT更新監視
-    firebaseAuth.onIdTokenChanged(async (user) => {
-      if (!user) {
-        window.localStorage.removeItem('access_token');
-      } else {
+    const auth = getAuth();
+    onIdTokenChanged(auth, async (user) => {
+      if (user) {
         const token = await user.getIdToken();
         window.localStorage.setItem('access_token', token);
+        console.log('change token', new Date());
+        console.log(token);
+      } else {
+        window.localStorage.removeItem('access_token');
       }
     });
   }, []);
+
+  // useEffect(() => {
+  //   // JWT更新監視
+  //   firebaseAuth.onIdTokenChanged((user) => {
+  //     if (user) {
+  //       const token = await user.getIdToken();
+  //       const result = await user.getIdTokenResult();
+  //       console.log(result.token);
+  //       console.log(result.token === token);
+  //       window.localStorage.setItem('access_token', token);
+  //       console.log('change token', new Date());
+  //       console.log(token);
+  //     } else {
+  //       window.localStorage.removeItem('access_token');
+  //     }
+  //   });
+  // }, []);
 
   return (
     <Provider store={store}>
