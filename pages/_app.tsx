0a4e4212d922ffd49421133172d42ff8c1ffffbe
tateno <gd28.c.tateno@gmail.com>
2022-01-29 15:42:53 +0900

    不要箇所削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { store } from '../store';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import { firebaseAuth } from '../firebase/config';
 import { useEffect } from 'react';
-import { resetAuth, updateToken } from '../store/auth';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // const dispatch = useDispatch();
-
   useEffect(() => {
     // JWT更新監視
     firebaseAuth.onIdTokenChanged(async (user) => {
       if (!user) {
-        // dispatch(resetAuth());
         window.localStorage.removeItem('access_token');
       } else {
         const token = await user.getIdToken();
         window.localStorage.setItem('access_token', token);
-        // dispatch(updateToken({ user }));
       }
     });
   }, []);
